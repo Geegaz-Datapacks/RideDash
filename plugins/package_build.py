d9fa327df9f7c76570265af5e2b7e90fdd9889a3
geegaz <geegazrd@gmail.com>
2026-02-06 14:16:04 +0100

    changed packaging plugin & added publishing config
    - the `package_build` plugin can now output a "combined" build with both data & assets
    - moved `package_build` options to the publishing config
    - the new publishing config handles versions along with building, and in the future uploading builds to all platforms

--- plugins/package_build.py
+++ plugins/package_build.py
@@ -1,31 +1,65 @@
 from beet import Context, PluginOptions, configurable
+from beet.contrib.autosave import Autosave
 from pathlib import Path
 from typing import Optional
+from zipfile import ZipFile
 
+class PackageOptions(PluginOptions):
+  # Should the plugin output this package ?
+  package: bool # Required
+  zipped: Optional[bool] = False
 
 class BuildOptions(PluginOptions):
   directory: Optional[Path] = None
-  zipped: Optional[bool] = True
-  # use_version: Optional[bool] = True
+  separate: PackageOptions
+  combined: PackageOptions
+  # In case of duplicate extra files when combining packages,
+  # should the plugin use the files from assets or data ?
+  use_assets_extra: Optional[bool] = True
 
 def beet_default(ctx: Context):
-  ctx.require(package_build)
+  # Ensure this runs after all other plugins
+  autosave = ctx.inject(Autosave)
+  autosave.add_link(package_build)
 
 @configurable(validator=BuildOptions)
 def package_build(ctx: Context, opts: BuildOptions):
   path = opts.directory or ctx.output_directory
   if path is None:
     return # Skip if no output path has been provided
-
-  # if opts.use_version:
-  #   path /= ctx.project_version
   
   path /= ctx.project_version
 
-  pack_name = f"{ctx.project_id}_{ctx.project_version}"
-  if opts.zipped:
-    ctx.data.save(path=path/f"{pack_name}_datapack.zip", zipped=True, overwrite=True)
-    ctx.assets.save(path=path/f"{pack_name}_resourcepack.zip", zipped=True, overwrite=True)
-  else:
-    ctx.data.save(path=path/f"{pack_name}_datapack", overwrite=True)
-    ctx.assets.save(path=path/f"{pack_name}_resourcepack", overwrite=True)
+  if opts.separate.package:
+    # Package the datapack & resourcepack into separate directories/zip files
+    assets_path = path / f"{ctx.project_id}_{ctx.project_version}_resourcepack"
+    data_path = path / f"{ctx.project_id}_{ctx.project_version}_datapack"
+    if opts.separate.zipped:
+      ctx.assets.save(path=assets_path.with_suffix(".zip"), zipped=True, overwrite=True)
+      ctx.data.save(path=data_path.with_suffix(".zip"), zipped=True, overwrite=True)
+    else:
+      ctx.assets.save(path=assets_path, overwrite=True)
+      ctx.data.save(path=data_path, overwrite=True)
+  
+  if opts.combined.package:
+    # Remove duplicate extra files (pack.mcmeta, pack.png...)
+    if opts.use_assets_extra:
+      remove_source = ctx.assets.extra
+      remove_target = ctx.data.extra
+    else:
+      remove_source = ctx.data.extra
+      remove_target = ctx.assets.extra
+    
+    for extra in remove_source:
+      if extra in remove_target:
+        remove_target.pop(extra)
+    
+    # Package both data & assets into a single directory/zip file
+    combined_path = path / f"{ctx.project_id}_{ctx.project_version}"
+    if opts.combined.zipped:
+      with ZipFile(combined_path.with_suffix(".zip"), 'w') as zip:
+        ctx.assets.dump(zip)
+        ctx.data.dump(zip)
+    else:
+      ctx.assets.dump(combined_path)
+      ctx.data.dump(combined_path)
